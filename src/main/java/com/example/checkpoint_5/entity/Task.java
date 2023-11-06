cb578e75f7f6b54671f01a5f3187f6f21b31b667
Guilherme <gui.oliveira1104@outlook.com>
2023-11-06 19:46:37 -0300

    Primeiro commit

---
+++ src/main/java/com/example/checkpoint_5/entity/Task.java
@@ -0,0 +1,20 @@
+package com.example.checkpoint_5.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@Table(name ="table_task")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String description;
+    private String status;
+    private LocalDate dueDate;
+}
